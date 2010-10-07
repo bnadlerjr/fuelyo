26ee0dff5e15e2bc96aef14bcf04b36173b16040
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-07 07:05:56 -0400

    Spike Zeep panel.

--- fuelyo.rb
+++ fuelyo.rb
@@ -6,3 +6,20 @@ require 'sinatra'
 get '/' do
   'Welcome to Fuelyo!'
 end
+
+get '/panel' do
+  erb :panel
+end
+
+enable :inline_templates
+
+__END__
+
+@@ panel
+<iframe 
+  style="width: 100%; height: 300px; border: none;" 
+  frameborder="0" allowtransparency="0"
+  id="zeep_mobile_settings_panel" 
+  src="https://www.zeepmobile.com/subscription/settings?api_key=5f02d2d6-97e8-4316-83b6-f5ee69b6af26&user_id=[1044]"
+>
+</iframe>  
