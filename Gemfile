711d6b4abb3d99f4353e1acdbc0386248b19e5dc
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-07 22:10:09 -0400

    First try at DataMapper.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
+gem 'datamapper'
 
 group :development do
   gem 'shotgun'
--- fuelyo.rb
+++ fuelyo.rb
@@ -2,6 +2,40 @@ require 'bundler'
 Bundler.setup
 
 require 'sinatra'
+require 'datamapper'
+
+configure :production do
+  DataMapper::setup(:default, ENV['DATABASE_URI'])
+end
+
+class FuelRecord
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :user_id, Integer
+  property :odometer, Integer
+  property :price, Float
+  property :gallons, Float
+  property :miles_per_gallon, Float
+  property :created_at, DateTime, :default => lambda { |r,p| p = Time.now }
+
+  before :save, :calculate_miles_per_gallon
+
+  def self.create_from_sms(user_id, odometer, price, gallons)
+    FuelRecord.create(:user_id => user_id, 
+                      :odometer => odometer, 
+                      :price => price, 
+                      :gallon => gallons)
+  end
+
+  private
+
+  def calculate_miles_per_gallon
+    miles_per_gallon = odometer / gallons
+  end
+end
+
+FuelRecord.auto_migrate!
 
 get '/' do
   'Welcome to Fuelyo!'
@@ -13,11 +47,9 @@ end
 
 post '/incoming' do
   content_type 'text/plain'
-  "SMS #{params[:body]}"
-end
-
-get '/env' do
-  erb :env
+  r = FuelRecord.create_from_sms(1, 2, 3.4, 4.5)
+  "Successfully saved fuel record. Current MPG is #{r.miles_per_gallon}."
+#  "SMS #{params[:body]}"
 end
 
 enable :inline_templates
@@ -32,10 +64,3 @@ __END__
   src="https://www.zeepmobile.com/subscription/settings?api_key=5f02d2d6-97e8-4316-83b6-f5ee69b6af26&user_id=[1044]"
 >
 </iframe>  
-
-@@ env
-<ul>
-<% ENV.each do |k,v| %>
-<li><%= "#{k} => #{v} %></li>
-<% end %>
-</ul>
