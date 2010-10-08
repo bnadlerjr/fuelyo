15e743582ee92665db6263a7192fee852da21036
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-07 22:56:17 -0400

    Fix gallons property typo

--- fuelyo.rb
+++ fuelyo.rb
@@ -25,7 +25,7 @@ class FuelRecord
    FuelRecord.create(:user_id => user_id, 
                      :odometer => odometer, 
                      :price => price, 
-                     :gallon => gallons)
+                     :gallons => gallons)
  end
 
  private
@@ -52,7 +52,7 @@ end
 post '/incoming' do
   content_type 'text/plain'
   r = FuelRecord.create_from_sms(1, 2, 3.4, 4.5)
-  "Successfully saved fuel record. Current MPG is #{r.inspect}."
+  "Successfully saved fuel record. Current MPG is #{r.miles_per_gallon}."
 end
 
 get '/env' do
