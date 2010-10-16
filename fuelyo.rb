7f48d1170ddec0bb8f932987b79d26e1df566277
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-16 15:00:24 -0400

    Simple index view for fuel records.

--- fuelyo.rb
+++ fuelyo.rb
@@ -1,9 +1,14 @@
+require 'rubygems'
 require 'bundler'
 Bundler.setup
 
 require 'sinatra'
 require 'datamapper'
 require 'models/fuel_record'
+
+configure :development do
+  DataMapper::setup(:default, 'sqlite::memory:')
+end
 
 configure :test do
   DataMapper::setup(:default, 'sqlite::memory:')
@@ -34,7 +39,8 @@ get '/panel' do
 end
 
 get '/records' do
-  FuelRecord.all
+  @records = FuelRecord.all
+  erb :records
 end
 
 post '/incoming' do
@@ -63,3 +69,33 @@ __END__
   src="https://www.zeepmobile.com/subscription/settings?api_key=5f02d2d6-97e8-4316-83b6-f5ee69b6af26&user_id=[1044]"
 >
 </iframe>  
+
+@@ records
+<html>
+  <head></head>
+  <body>
+    <h1>Fuelyo</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Date / Time</th>
+          <th>Odometer</th>
+          <th>Price</th>
+          <th>Gallons</th>
+          <th>MPG</th>
+        </tr>
+      </thead>
+      <tbody>
+        <% @records.each do |r| %>
+          <tr>
+            <td><%= r.created_at %></td>
+            <td><%= r.odometer %></td>
+            <td><%= r.price %></td>
+            <td><%= r.gallons %></td>
+            <td><%= r.miles_per_gallon %></td>
+          </tr>
+        <% end %>
+      </tbody>
+    </table>
+  </body>
+</html>
