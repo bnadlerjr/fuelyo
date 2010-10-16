f51cca810af3a1e482b8e2e981f3c8a9c331fb4e
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-16 14:33:33 -0400

    Test for successfully saving a fuel record. MPG is formatted to 2 decimals.

--- test/test_app.rb
+++ test/test_app.rb
@@ -7,12 +7,17 @@ class TestApp < Test::Unit::TestCase
     Sinatra::Application
   end
 
+  def setup
+    FuelRecord.destroy
+    assert_equal 0, FuelRecord.all.count
+  end
+
   def test_root
     get '/'
     assert_equal 'Welcome to Fuelyo!', last_response.body
   end
 
-  def test_incoming_message
+  def test_incoming_first_fuel_record
     post '/incoming', {
         "body"       => "150 2.99 15",
         "app_id"     => "1094",
@@ -24,5 +29,21 @@ class TestApp < Test::Unit::TestCase
     }
     
     assert_equal "Great! You've saved your first fuel record. The next time you send fuel information we'll be able to calculate your MPG.", last_response.body
+  end
+
+  def test_incoming_existing_fuel_record
+    FuelRecordFactory.create
+    post '/incoming', {
+        "body"       => "150 2.99 15",
+        "app_id"     => "1094",
+        "uid"        => "[1044]",
+        "sms_prefix" => "fuelyo",
+        "event"      => "MO",
+        "min"        => "test:2137",
+        "short_code" => "test:48147"
+    }
+
+    assert_equal "Successfully saved fuel record. Current MPG is 3.33.", 
+                 last_response.body
   end
 end
