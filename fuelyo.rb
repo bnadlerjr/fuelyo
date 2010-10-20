ff47cc0f429465d5b386d270c6a4a529b5709b43
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-10-20 14:52:08 -0400

    * Get rid of inline templates. * Move ZeepMobile panel into sidebar of records view.

--- fuelyo.rb
+++ fuelyo.rb
@@ -37,10 +37,6 @@ get '/' do
   erb :index
 end
 
-get '/panel' do
-  erb :panel
-end
-
 get '/records' do
   user = User.get(session['user_id'])
   @averages = user.fuel_history
@@ -80,43 +76,3 @@ end
 enable :inline_templates
 
 __END__
-
-@@ panel
-<iframe 
-  style="width: 100%; height: 300px; border: none;" 
-  frameborder="0" allowtransparency="0"
-  id="zeep_mobile_settings_panel" 
-  src="https://www.zeepmobile.com/subscription/settings?api_key=5f02d2d6-97e8-4316-83b6-f5ee69b6af26&user_id=[1044]"
->
-</iframe>  
-
-@@ records
-<html>
-  <head></head>
-  <body>
-    <h1>Fuelyo</h1>
-    <div><%= @averages.join(',') %></div>
-    <table>
-      <thead>
-        <tr>
-          <th>Date / Time</th>
-          <th>Odometer</th>
-          <th>Price</th>
-          <th>Gallons</th>
-          <th>MPG</th>
-        </tr>
-      </thead>
-      <tbody>
-        <% @records.each do |r| %>
-          <tr>
-            <td><%= r.created_at.strftime("%Y-%m-%d at %I:%M%p") %></td>
-            <td><%= r.odometer %></td>
-            <td><%= r.price %></td>
-            <td><%= r.gallons %></td>
-            <td><%= sprintf('%.2f', r.miles_per_gallon) %></td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
-  </body>
-</html>
