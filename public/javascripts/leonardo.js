fe4b841bb1cdf5a4926cc6568c2f38a95da6f0f2
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-11-30 22:33:19 -0500

    Update version of Leonardo.js

--- public/javascripts/leonardo.js
+++ public/javascripts/leonardo.js
@@ -2,52 +2,76 @@
 /*global Raphael */
 
 /**
- * Raphael helper function for drawing a horizontal line.
- *
- * x: Value of the x-coordinate
- * y: Value of the y-coordinate
- * width: Length of the line
+ * Raphael helper functions. Namespaced to prevent trampling the main Raphael
+ * namespace.
  */
-Raphael.fn.hLine = function (x, y, width) {
-    return this.path(["M", x, y, "h", width]);
-};
+Raphael.fn.leonardo = {
+    /**
+     * Draws a horizontal line.
+     *
+     * Arguments
+     * x:
+     *     Value of the x-coordinate
+     *
+     * y:
+     *     Value of the y-coordinate
+     *
+     * width:
+     *     Length of the line
+     */
+    hLine: function (x, y, width) {
+        return this.path(["M", x, y, "h", width]);
+    },
 
-/**
- * Raphael helper function for drawing a vertical line.
- *
- * x: Value of the x-coordinate
- * y: Value of the y-coordinate
- * height: Length of the line
- */
-Raphael.fn.vLine = function (x, y, height) {
-    return this.path(["M", x, y, "v", height]);
-};
+    /**
+     * Draws a vertical line.
+     *
+     * Arguments
+     * x:
+     *     Value of the x-coordinate
+     *
+     * y:
+     *     Value of the y-coordinate
+     *
+     * height:
+     *     Length of the line
+     */
+    vLine: function (x, y, height) {
+        return this.path(["M", x, y, "v", height]);
+    },
 
-/**
- * Raphael helper function for drawing a line between two points.
- *
- * x1, y1: Coords for 'from' point
- * x2, y2: Coords for 'to' point
- */
-Raphael.fn.line = function (x1, y1, x2, y2) {
-    return this.path(["M", x1, y1, "L", x2, y2]);
-};
+    /**
+     * Draws drawing a line between two points.
+     *
+     * Arguments
+     * x1, y1:
+     *     Coords for 'from' point
+     *
+     * x2, y2:
+     *     Coords for 'to' point
+     */
+    line: function (x1, y1, x2, y2) {
+        return this.path(["M", x1, y1, "L", x2, y2]);
+    },
 
-/**
- * Raphael helper function for drawing a line from the current point to the
- * specified coordinates. The first time this function is called, x and y are
- * cached. On subsequent calls, they are again cached after drawing the line.
- *
- * NOTE: There is currently no way to reset the cache for the last coordinates.
- * Not sure if we're going to need that yet.
- *
- * x, y: Coords to draw the line to.
- */
-Raphael.fn.lineTo = function (x, y) {
-    if (this.cache) {
-        this.path(["M", this.cache.lastX, this.cache.lastY, "L", x, y]);
+    /**
+     * Draws a line from the current point to the specified coordinates. The first
+     * time this function is called, x and y are cached. On subsequent calls, they
+     * are again cached after drawing the line.
+     *
+     * NOTE: There is currently no way to reset the cache for the last coordinates.
+     * Not sure if we're going to need that yet.
+     *
+     * Arguments
+     * x, y:
+     *     Coords to draw the line to.
+     */
+    lineTo: function (x, y) {
+        if (this.cache) {
+            this.path(["M", this.cache.lastX, this.cache.lastY, "L", x, y]);
+        }
+        this.cache = { lastX: x, lastY: y };
     }
-    this.cache = { lastX: x, lastY: y };
 };
 
 var Leonardo = {};
@@ -65,7 +89,7 @@ Leonardo = (function () {
             'width'              : 300,
             'height'             : 200,
             'padding'            : 10,
-            'barWidth'           : 5,
+            'barWidth'           : 6,
             'tickSize'           : 5,
             'drawBorder'         : true,
             'plotAreaPercentage' : 0.9,
@@ -117,21 +141,22 @@ Leonardo = (function () {
     /**
      * Initializes graph by calculating the plot area and scale values.
      */
-    function init(domID, max, options) {
+    function init(domID, max, num_labels, options) {
         opts = extend(opts, options || {});
         maxValue = max;
-        
-        var paddedWidth  = opts.width  - (opts.padding * 2),
-            paddedHeight = opts.height - (opts.padding * 2),
+
+        var paddedWidth        = opts.width  - (opts.padding * 2),
+            paddedHeight       = opts.height - (opts.padding * 2),
             axisAreaPercentage = 1 - opts.plotAreaPercentage,
             ratio              = maxValue / opts.scaleSize,
-            i = opts.scaleSize;
+            i                  = opts.scaleSize;
 
         // Calculate size of plot area
-        plotArea.x      = axisAreaPercentage * paddedWidth + opts.padding;
-        plotArea.y      = opts.padding;
-        plotArea.width  = opts.plotAreaPercentage * paddedWidth;
-        plotArea.height = paddedHeight - (axisAreaPercentage / 2 * opts.height);
+        plotArea.x       = axisAreaPercentage * paddedWidth + opts.padding;
+        plotArea.y       = opts.padding;
+        plotArea.width   = opts.plotAreaPercentage * paddedWidth;
+        plotArea.height  = paddedHeight - (axisAreaPercentage / 2 * opts.height);
+        plotArea.xOffset = (plotArea.width / num_labels / 2) - opts.padding;
 
         // Calculate scale values
         // TODO: Round the scale values
@@ -173,14 +198,15 @@ Leonardo = (function () {
      * Draw the x-axis for the chart.
      */
     function drawXAxis(labels) {
-        paper.hLine(plotArea.x, plotArea.y + plotArea.height, plotArea.width);
+        paper.leonardo.hLine(plotArea.x, plotArea.y + plotArea.height, plotArea.width);
 
         // TODO: Get rid of magic number. Base labelText on font size.
         var labelY = plotArea.y + plotArea.height,
             labelTextY = labelY + (2 * opts.tickSize);
 
         plot(labels, function (x, y, label) {
-            paper.vLine(x, labelY, opts.tickSize);
+            x += plotArea.xOffset;
+            paper.leonardo.vLine(x, labelY, opts.tickSize);
             paper.text(x, labelTextY, label);
         });
     }
@@ -189,16 +215,33 @@ Leonardo = (function () {
      * Draw the y-axis for the chart.
      */
     function drawYAxis() {
-        paper.vLine(plotArea.x, plotArea.y, plotArea.height);
+        paper.leonardo.vLine(plotArea.x, plotArea.y, plotArea.height);
 
         // TODO: Get rid of magic number. Base text on font size instead.
         var valueX = plotArea.x - opts.tickSize,
             valueTextX = plotArea.x - (3 * opts.tickSize);
 
         plot(scaleValues, function (x, y, value) {
-            paper.hLine(valueX, y, opts.tickSize);
+            paper.leonardo.hLine(valueX, y, opts.tickSize);
             paper.text(valueTextX, y, value);
         });
+    }
+
+    /**
+     * Returns the length of a data series. Series is an object that looks like
+     * an associative array. This method returns the number of "keys".
+     */
+    function getSeriesLength(series) {
+        var size = 0,
+            key;
+
+        for (key in series) {
+            if (series.hasOwnProperty(key)) {
+                size += 1;
+            }
+        }
+
+        return size;
     }
 
     /**
@@ -208,20 +251,23 @@ Leonardo = (function () {
         /**
          * Draws a column based chart.
          *
-         * Arguments:
+         * Arguments
+         * domID:
+         *     ID for a DOM element. This element will contain the graph.
          *
-         * domID:   ID for a DOM element. This element will contain the graph.
+         * series:
+         *     Key-value pair where the key is the name of the data series and the
+         *     value is an array of values for the series.
          *
-         * series:  Key-value pair where the key is the name of the data
-         *          series and the value is an array of values for the series.
+         * labels:
+         *     Array of strings to use as labels.
          *
-         * labels:  Array of strings to use as labels.
-         *
-         * options: Key-value pairs representing various chart options. See
-         *          default options above for a list.
+         * options:
+         *     Key-value pairs representing various chart options. See default
+         *     options above for a list.
          */
         columnChart: function (domID, series, labels, options) {
-            init(domID, getMaxValue(series), options || {});
+            init(domID, getMaxValue(series), labels.length, options || {});
 
             if (opts.drawBorder) {
                 drawBorder(opts.width, opts.height);
@@ -231,22 +277,41 @@ Leonardo = (function () {
             drawYAxis();
 
             // Draw series bars
-            // TODO: Change label and tick positions so that they are centered
-            // in the seriesWidth.
             // TODO: barWidth should actually be calculated according to
             // seriesWidth.
             var adjustedHeight = plotArea.height + opts.padding,
                 seriesOffset = 0,
-                item;
+                seriesLength = getSeriesLength(series),
+                halfBarWidth = opts.barWidth / 2,
+                item,
+                color;
 
+            Raphael.getColor.reset();
+            color = Raphael.getColor();
             for (item in series) {
                 if (series.hasOwnProperty(item)) {
                     plot(series[item], function (x, y, value) {
-                        paper.rect(x + seriesOffset, y,
-                            opts.barWidth, adjustedHeight - y);
+                        var localColor = color;
+                        x += seriesOffset - halfBarWidth;
+
+                        // Only one data series so center the bar over the tick
+                        // mark.
+                        if (seriesLength === 1) {
+                            x += plotArea.xOffset;
+                        }
+
+                        paper.rect(x, y, opts.barWidth, adjustedHeight - y).hover(
+                            function (event) {
+                                    this.attr({fill: "gray"});
+                                }, function (event) {
+                                this.attr({fill: localColor});
+                            }).attr({title: item + ": " + value, fill: localColor});
                     });
 
                     seriesOffset += opts.barWidth;
+                    if (!(opts.singleSeriesColor)) {
+                        color = Raphael.getColor();
+                    }
                 }
             }
         },
@@ -254,20 +319,24 @@ Leonardo = (function () {
         /**
          * Draws a line chart.
          *
-         * Arguments:
+         * Arguments
          *
-         * domID:   ID for a DOM element. This element will contain the graph.
+         * domID:
+         *     ID for a DOM element. This element will contain the graph.
          *
-         * series:  Key-value pair where the key is the name of the data
-         *          series and the value is an array of values for the series.
+         * series:
+         *     Key-value pair where the key is the name of the data series and the
+         *     value is an array of values for the series.
          *
-         * labels:  Array of strings to use as labels.
+         * labels:
+         *     Array of strings to use as labels.
          *
-         * options: Key-value pairs representing various chart options. See
-         *          default options above for a list.
+         * options:
+         *     Key-value pairs representing various chart options. See default
+         *     options above for a list.
          */
         lineChart: function (domID, series, labels, options) {
-            init(domID, getMaxValue(series), options || {});
+            init(domID, getMaxValue(series), labels.length, options || {});
 
             if (opts.drawBorder) {
                 drawBorder(opts.width, opts.height);
@@ -280,7 +349,7 @@ Leonardo = (function () {
             for (var item in series) {
                 if (series.hasOwnProperty(item)) {
                     plot(series[item], function (x, y, value) {
-                        paper.lineTo(x, y);
+                        paper.leonardo.lineTo(x + plotArea.xOffset, y);
                     });
                 }
             }
@@ -289,55 +358,62 @@ Leonardo = (function () {
         /**
          * Draws a sparkline chart. Only accepts one data series.
          *
-         * Arguments:
+         * Arguments
+         * domID:
+         *     ID for a DOM element. This element will contain the graph.
          *
-         * domID:   ID for a DOM element. This element will contain the graph.
+         * data:
+         *     Array of data values.
          *
-         * data:    Array of data values.
-         *
-         * options: Key-value pairs representing various chart options. See
-         *          default options above for a list.
+         * options:
+         *     Key-value pairs representing various chart options. See default
+         *     options above for a list.
          */
         sparkline: function (domID, data, options) {
             opts = extend(opts, options || {});
-            
+
             plotArea.x      = opts.padding;
             plotArea.width  = opts.width  - (opts.padding * 2);
             plotArea.height = opts.height;
-            
+
             maxValue = Math.max.apply(Math, data);
             paper = new Raphael(domID, opts.width, opts.height);
 
-            plot(data, function (x, y) {
-                paper.lineTo(x, y);
+            plot(data, function (x, y, value) {
+                if (value === 0) {
+                    y = plotArea.height;
+                }
+                paper.leonardo.lineTo(x, y);
             });
         },
-        
+
         /**
          * Draws a single horizontal bar.
          *
-         * Arguments:
+         * Arguments
+         * domID:
+         *     ID for a DOM element. This element will contain the graph.
          *
-         * domID:               ID for a DOM element. This element will contain the
-         *                      graph.
+         * percentageAsDecimal:
+         *     Percentage value for the bar, given in decimal format.
          *
-         * percentageAsDecimal: Percentage value for the bar, given in decimal
-         *                      format.
-         *
-         * options:             Key-value pairs representing various chart options.
-         *                      See default options above for a list.
+         * options:
+         *     Key-value pairs representing various chart options.  See default
+         *     options above for a list.
          */
         oneBar: function (domID, percentageAsDecimal, options) {
             opts = extend(opts, options || {});
-            
+
             var paddedHeight = opts.height - 2;
-            
+
+            Raphael.getColor.reset();
+
             paper = new Raphael(domID, opts.width, opts.height);
-            paper.rect(0, 1, percentageAsDecimal * opts.width, paddedHeight);
-            
+            paper.rect(0, 1, percentageAsDecimal * opts.width, paddedHeight).attr({fill: Raphael.getColor()});
+
             if (opts.showPercentage) {
-                paper.text(opts.padding * 2,
-                    paddedHeight / 2, (percentageAsDecimal * 100).toFixed(0) + "%");
+                paper.text(2,
+                    paddedHeight / 2, (percentageAsDecimal * 100).toFixed(0) + "%").attr({'text-anchor': 'start'});
             }
         }
     };
